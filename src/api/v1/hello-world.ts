ef64e478f5940d2864a3c44dafea4f216b904e3d
ljourm <ljourm@gmail.com>
2025-03-01 22:59:21 +0900

    feat: APIのローカル実行方法の整備

--- src/api/v1/hello-world.ts
+++ src/api/v1/hello-world.ts
@@ -1,10 +1,15 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
-export const handler: APIGatewayProxyHandler = async () => {
-  console.log("Hello, World!");
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const pathMessage = event.queryStringParameters?.message || "none";
+  const bodyMessage = JSON.parse(event.body || "{}").message || "none";
+
+  const output = `Hello! You said: ${pathMessage} and ${bodyMessage}`;
+
+  console.log(output);
 
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello, World!"),
+    body: JSON.stringify(output),
   };
 };
