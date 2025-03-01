dd28f483af8533be711aa896377c64452e479f64
ljourm <ljourm@gmail.com>
2025-03-02 01:00:47 +0900

    feat: 環境をprd/stgに切り分け

--- src/api/v1/hello-world.ts
+++ src/api/v1/hello-world.ts
@@ -3,8 +3,10 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async (event) => {
   const pathMessage = event.queryStringParameters?.message || "none";
   const bodyMessage = JSON.parse(event.body || "{}").message || "none";
+  const env = process.env.ENV;
+  const hoge = process.env.HOGE;
 
-  const output = `Hello! You said: ${pathMessage} and ${bodyMessage}`;
+  const output = { pathMessage, bodyMessage, env, hoge };
 
   console.log(output);
 
